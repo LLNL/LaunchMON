61a841487b5b6b78e1af10f4bd891c3fe500d9e6
Dong Ahn <ahn1@llnl.gov>
2009-08-12 16:57:30 +0000

    added thread state tracking support for better PT control

--- launchmon/src/linux/lmon_api/lmon_be_comm.cxx
+++ launchmon/src/linux/lmon_api/lmon_be_comm.cxx
@@ -52,10 +52,30 @@
 #error This source file requires a LINUX OS
 #endif
 
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <iostream>
+#if HAVE_STDIO_H
+# include <cstdio>
+#else
+# error stdio.h is required
+#endif
+
+#if HAVE_STDLIB_H
+# include <cstdlib>
+#else
+# error stdlib.h is required
+#endif
+
+#if HAVE_STRING_H
+# include <string.h>
+#else
+# error string.h is required
+#endif
+
+#if HAVE_IOSTREAM
+# include <iostream>
+#else
+# error iostream is required
+#endif
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
