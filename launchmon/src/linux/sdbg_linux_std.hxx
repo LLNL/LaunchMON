f9b559adff22bc13c956acab207c9bc6f61f4f8c
Dong Ahn <ahn1@llnl.gov>
2009-08-12 16:58:02 +0000

    added thread state tracking support for better PT control

--- launchmon/src/linux/sdbg_linux_std.hxx
+++ launchmon/src/linux/sdbg_linux_std.hxx
@@ -26,34 +26,122 @@
  *--------------------------------------------------------------------------------			
  *
  *  Update Log:
+ *        Aug  10 2009 DHA: Added more comments
  *        Mar  06 2008 DHA: Deprecate GLUESYM support
  *        Mar  11 2008 DHA: Added PowerPC support (BlueGene FEN)
  *        Feb  09 2008 DHA: Added LLNS Copyright
- *        Aug  15 2007 DHA: Change C macroes to C++ style: const char* const 
+ *        Aug  15 2007 DHA: Change C macroes to C++ style: const char * const 
  *        Jan  09 2006 DHA: Linux X86-64 support
- *        Mar  22 2006 DHA: Created file. 
- *                   
+ *        Mar  22 2006 DHA: Created file.
+ *
  */ 
 
 #ifndef SDBG_LINUX_STD_HXX
 #define SDBG_LINUX_STD_HXX 1
 
+//! LAUNCH_BREAKPOINT:
+/*!
+    Symbol for the dummy function that gets invoked
+    by the RM to notify state changes of the target job
+*/
 const char * const LAUNCH_BREAKPOINT        = "MPIR_Breakpoint";
+
+//! LAUNCH_BEING_DEBUG:
+/*!
+    Symbol that tells the RM whether it is traced or not
+*/
 const char * const LAUNCH_BEING_DEBUG       = "MPIR_being_debugged";
+
+//! LAUNCH_DEBUG_STATE:
+/*!
+    Symbol that tells the engine the state of the job when
+    LAUNCH_BREAKPOINT is called
+*/
 const char * const LAUNCH_DEBUG_STATE       = "MPIR_debug_state";
+
+//! LAUNCH_DEBUG_GATE:
+/*!
+    Symbol to tells the job to gate the starting of the job
+*/
 const char * const LAUNCH_DEBUG_GATE        = "MPIR_debug_gate";
+
+//! LAUNCH_PROCTABLE:
+/*!
+    Symbol that holds an array of MPI process table
+*/
 const char * const LAUNCH_PROCTABLE         = "MPIR_proctable";
+
+//! LAUNCH_PROCTABLE:
+/*!
+    Symbol that holds the size of LAUNCH_PROCTABLE
+*/
 const char * const LAUNCH_PROCTABLE_SIZE    = "MPIR_proctable_size";
+
+//! LAUNCH_ACQUIRED_PREMAIN:
+/*!
+    Symbol that tells whether a debugger can force the main 
+    for the first attach point
+*/
 const char * const LAUNCH_ACQUIRED_PREMAIN  = "MPIR_acquired_pre_main";
+
+//! LAUNCH_EXEC_PATH:
+/*!
+    Symbol that provides the tool daemon exectuable path to the RM
+*/
 const char * const LAUNCH_EXEC_PATH         = "MPIR_executable_path";
+
+//! LAUNCH_EXEC_PATH:
+/*!
+    Symbol that provides the tool daemons with their arguments
+*/
 const char * const LAUNCH_SERVER_ARGS       = "MPIR_server_arguments";
+
+//! LOADER_BP_SYM:
+/*!
+    Symbol within the dynamic linker, which gets invoked on shared library 
+    load
+*/
 const char * const LOADER_BP_SYM            = "_dl_debug_state";
+
+//! LOADER_R_DEBUG:
+/*!
+    Symbol within the dynamic linker, which holds the current link map
+*/
 const char * const LOADER_R_DEBUG           = "_r_debug";
+
+//! LOADER_START:
+/*!
+    Symbol within the dynamic linker, which is the first function to be
+    called after exec
+*/
 const char * const LOADER_START             = "_start";
+
+//! NPTL_CREATE_SYM:
+/*!
+    Symbol within the POSIX thread library, which is invokded when a 
+    new thread is created
+*/
 const char * const NPTL_CREATE_SYM          = "__nptl_create_event";
+
+//! NPTL_CREATE_SYM:
+/*!
+    Symbol within the POSIX thread library, which is invokded when a 
+    thread dies/exits
+*/
 const char * const NPTL_DEATH_SYM           = "__nptl_death_event";
+
+//! FORK_SYM:
+/*!
+    Symbol within the libc for the normal fork call 
+*/
 const char * const FORK_SYM                 = "__fork";
+
+//! VFORK_SYM:
+/*!
+    Symbol within the libc for the virtual fork call 
+*/
 const char * const VFORK_SYM                = "__vfork";
+
 const char * const LIBC_IDEN                = "libc.";
 const char * const LIBPTHREAD_IDEN          = "libpthread."; 
 const char * const RESOURCE_HANDLER_SYM     = "totalview_jobid";
@@ -66,22 +154,26 @@ const int MAX_STRING_SIZE                   = 1024;
 
 #if X86_ARCHITECTURE
 
-/* 
- * 
- * insert linux X86 architecture macroes here ...
- *
- *
- */
+  // 
+  // 
+  // insert linux X86 architecture macros here ...
+  //
+  //
+  //
 
-typedef u_int32_t                             T_VA;
-typedef u_int32_t                             T_WT;
-typedef u_int32_t                             T_IT;
-typedef struct user_regs_struct               T_GRS;
-typedef struct user_fpregs_struct             T_FRS;
+  typedef u_int32_t                             T_VA;
+  typedef u_int32_t                             T_WT;
+  //
+  // For the purpose of what we are doing, the fixed-length
+  // T_IT is fine to represent an instruction
+  //
+  typedef u_int32_t                             T_IT;
+  typedef struct user_regs_struct               T_GRS;
+  typedef struct user_fpregs_struct             T_FRS;
 
-const T_VA T_TRAP_INSTRUCTION               = 0x000000cc;
-const T_VA T_BLEND_MASK                     = 0xffffff00;
-const T_VA T_UNINIT_HEX                     = 0xdeadbeef;
+  const T_IT T_TRAP_INSTRUCTION               = 0x000000cc;
+  const T_IT T_BLEND_MASK                     = 0xffffff00;
+  const T_VA T_UNINIT_HEX                     = 0xdeadbeef;
 
 #define SDBG_LINUX_DFLT_INSTANTIATION T_VA, \
                                       T_WT, \
@@ -93,33 +185,38 @@ const T_VA T_UNINIT_HEX                     = 0xdeadbeef;
 
 #elif X86_64_ARCHITECTURE
 
-/*
- *
- * Insert linux X86-64 architecture macroes here ...
- *
- *
- */
+  // 
+  // 
+  // insert linux X86-64 architecture macros here ...
+  //
+  //
+  //
 
-#if BIT64 
-typedef u_int64_t                             T_VA;
-typedef u_int64_t                             T_WT;
-typedef u_int64_t                             T_IT;
-typedef struct user_regs_struct               T_GRS;
-typedef struct user_fpregs_struct             T_FRS;
-const T_VA T_TRAP_INSTRUCTION               = 0x00000000000000cc;
-const T_VA T_BLEND_MASK                     = 0xffffffffffffff00;
-const T_VA T_UNINIT_HEX                     = 0xdeadbeefdeadbeef;
-#else
-typedef u_int32_t                             T_VA;
-typedef u_int32_t                             T_WT;
-typedef u_int32_t                             T_IT;
-typedef struct user_regs_struct               T_GRS;
-typedef struct user_fpregs_struct             T_FRS;
-const T_VA T_TRAP_INSTRUCTION               = 0x000000cc;
-const T_VA T_BLEND_MASK                     = 0xffffff00;
-const T_VA T_UNINIT_HEX                     = 0xdeadbeef;
-#endif 
-
+# if BIT64
+  //
+  // if the target is 64 bit, use the following
+  //
+  typedef u_int64_t                             T_VA;
+  typedef u_int64_t                             T_WT;
+  typedef u_int64_t                             T_IT;
+  typedef struct user_regs_struct               T_GRS;
+  typedef struct user_fpregs_struct             T_FRS;
+  const T_IT T_TRAP_INSTRUCTION               = 0x00000000000000cc;
+  const T_IT T_BLEND_MASK                     = 0xffffffffffffff00;
+  const T_VA T_UNINIT_HEX                     = 0xdeadbeefdeadbeef;
+# else
+  //
+  // if the target is 32 bit, use the following
+  //
+  typedef u_int32_t                             T_VA;
+  typedef u_int32_t                             T_WT;
+  typedef u_int32_t                             T_IT;
+  typedef struct user_regs_struct               T_GRS;
+  typedef struct user_fpregs_struct             T_FRS;
+  const T_IT T_TRAP_INSTRUCTION               = 0x000000cc;
+  const T_IT T_BLEND_MASK                     = 0xffffff00;
+  const T_VA T_UNINIT_HEX                     = 0xdeadbeef;
+# endif // BIT64
 
 #define SDBG_LINUX_DFLT_INSTANTIATION T_VA, \
                                       T_WT, \
@@ -132,32 +229,35 @@ const T_VA T_UNINIT_HEX                     = 0xdeadbeef;
 
 #elif PPC_ARCHITECTURE
 
-/* 
- * 
- * Insert linux ppc architecture macroes here ...
- * BlueGene FEN class
- *
- */
+  // 
+  // 
+  // Insert linux ppc architecture macroes here ...
+  // BlueGene FEN architecture is covered here.
+  //
+  //
 
-#if BIT64 
-typedef u_int64_t                             T_VA;
-typedef u_int64_t                             T_WT;
-typedef u_int32_t                             T_IT;
-typedef struct pt_regs                        T_GRS;
-typedef struct pt_regs                        T_FRS;
-const T_IT T_TRAP_INSTRUCTION               = 0x7d821008;
-const T_IT T_BLEND_MASK                     = 0x00000000;
-const T_VA T_UNINIT_HEX                     = 0xdeadbeefdeadbeefULL;
-#else
-typedef u_int32_t                             T_VA;
-typedef u_int32_t                             T_WT;
-typedef u_int32_t                             T_IT;
-typedef struct pt_regs                        T_GRS;
-typedef struct pt_regs                        T_FRS;
-const T_VA T_TRAP_INSTRUCTION               = 0x7d821008;
-const T_VA T_BLEND_MASK                     = 0x00000000;
-const T_VA T_UNINIT_HEX                     = 0xdeadbeef;
-#endif 
+# if BIT64
+  //
+  // if the target is 64 bit, use the following
+  //
+  typedef u_int64_t                             T_VA;
+  typedef u_int64_t                             T_WT;
+  typedef u_int32_t                             T_IT;
+  typedef struct pt_regs                        T_GRS;
+  typedef struct pt_regs                        T_FRS;
+  const T_IT T_TRAP_INSTRUCTION               = 0x7d821008;
+  const T_IT T_BLEND_MASK                     = 0x00000000;
+  const T_VA T_UNINIT_HEX                     = 0xdeadbeefdeadbeefULL;
+# else
+  typedef u_int32_t                             T_VA;
+  typedef u_int32_t                             T_WT;
+  typedef u_int32_t                             T_IT;
+  typedef struct pt_regs                        T_GRS;
+  typedef struct pt_regs                        T_FRS;
+  const T_IT T_TRAP_INSTRUCTION               = 0x7d821008;
+  const T_IT T_BLEND_MASK                     = 0x00000000;
+  const T_VA T_UNINIT_HEX                     = 0xdeadbeef;
+# endif // BIT64
 
 #define SDBG_LINUX_DFLT_INSTANTIATION T_VA, \
                                       T_WT, \
@@ -166,6 +266,5 @@ const T_VA T_UNINIT_HEX                     = 0xdeadbeef;
                                       T_FRS,\
                                       td_thrinfo_t,\
                                       elf_wrapper
-
 #endif // ARCHITECTURES
 #endif // SDBG_LINUX_STD_HXX
