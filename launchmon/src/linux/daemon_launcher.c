46b41156d11eac9953725be18f38903edb36ed6b
Ramya Olichandran <ramya@cs.wisc.edu>
2010-04-21 17:14:52 +0000

    Work around daemon launcher

---
+++ launchmon/src/linux/daemon_launcher.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<errno.h>
+#include<string.h>
+
+int main(int argc,char** argv)
+{
+
+ // char filename[]="/tmp/work/ramya/AAAXXXXXX";
+ // int fd;
+ // fd=mkstemp(filename);
+ // write(fd,"here1",strlen("here1")+1);
+  
+  sleep(5);
+
+  int my_apid=atoi(argv[1]);
+  int no_daemon_opts=argc-10; /* first arg is apid, second is daemon_path followed by daemon opts and pmgr collective arguments */
+   
+  char* lib_path;
+  lib_path = getenv("LD_LIBRARY_PATH");
+
+  char add_lib_path[80];
+  sprintf(add_lib_path,":/var/spool/alps/%d/toolhelper%d",my_apid,my_apid);
+
+  strcat(lib_path,(const char*)add_lib_path);
+  setenv("LD_LIBRARY_PATH",(const char*)lib_path,1);
+
+
+  /*dynamic allocation of daemon argument list*/
+  char** myargv=(char**) malloc(argc*sizeof(char*));
+  myargv[0]=argv[2];/*daemon_path*/
+
+
+  /* Fill in the daemon opts ..how many ever they may be ***/
+  
+  int i;
+  for(i=1;i<=no_daemon_opts;i++)
+  {
+     myargv[i]=argv[2+i];
+        
+  }
+  
+  /** Fill in 7 pmgr_collective args***/
+  int pmgrargstart=i;
+  int j;
+  for(j=i;j<i+7;j++)
+  {
+     myargv[j]=argv[argc-7+j-pmgrargstart];
+  }
+  myargv[j]=NULL; 
+    
+  //write(fd,"here2",strlen("here2")+1);
+  //char execd[100];
+  //sprintf(execd, "execd %s\n", myargv[0]);
+  //write(fd,execd,strlen(execd) +1);
+
+  int ret=execv(myargv[0],myargv);
+
+  if(ret==-1)
+  {
+   //write(fd,"execv failed", strlen("execv failed")+1);
+   printf("error: %d\n",errno);
+  }
+  
+
+  return 0;
+}
+
+
