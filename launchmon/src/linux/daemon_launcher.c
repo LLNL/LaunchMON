c028db57c26fdbf9161ce698c104a7dbb301deb0
Ramya Olichandran <ramya@cs.wisc.edu>
2010-05-09 21:28:32 +0000

    Switch between cobo and pmgr in daemon_launcher

--- launchmon/src/linux/daemon_launcher.c
+++ launchmon/src/linux/daemon_launcher.c
@@ -6,24 +6,32 @@
 
 int main(int argc,char** argv)
 {
-/*
   char filename[]="/tmp/work/ramya/AAAXXXXXX";
-  int fd;
-  fd=mkstemp(filename);
-  write(fd,"here1",strlen("here1")+1);
+  int fd11;
   
-  printf("i am here\n");
+  fd11=mkstemp(filename);
+  
+  write(fd11,"here1",strlen("here1")+1);
+  
+  
 
-*/
-  
   int my_apid=atoi(argv[1]);
-  //int no_daemon_opts=argc-10; /* first arg is apid, second is daemon_path followed by daemon opts and pmgr collective arguments */
-   
-  int no_daemon_opts=argc-5;
-
+  unsigned int bootstrap_args=2;//this switch is on if its cobo and off if its pmgr ..we differentiate between cobo and pmgr based on number of args passed
+  int no_daemon_opts=0;  
   
+ 
+  if(argc == 10) 
+  {
+    //write(fd,"pmgr", strlen("pmgr")+1);
+    bootstrap_args=bootstrap_args+5;   //indicates it pmgr 
+    no_daemon_opts=argc-10; /* first arg is apid, second is daemon_path followed by daemon opts and pmgr collective arguments */
+  }
+  else if (argc == 5)
+  {
+    //write(fd,"cobo",strlen("cobo")+1);
+    no_daemon_opts=argc-5;
+  }
 
-   
   char* lib_path;
   lib_path = getenv("LD_LIBRARY_PATH");
 
@@ -49,20 +57,20 @@ int main(int argc,char** argv)
         
   }
   
-  /** Fill in 7 pmgr_collective args***/
+  /** Fill in 7 pmgr_collective/cobo args***/
   int pmgrargstart=i;
   int j;
-  for(j=i;j<i+2;j++)
+  for(j=i;j<i+bootstrap_args;j++)
   {
-     myargv[j]=argv[argc-2+j-pmgrargstart];
+     myargv[j]=argv[argc-bootstrap_args+j-pmgrargstart];
   }
   myargv[j]=NULL; 
     
-  //write(fd,"here2",strlen("here2")+1);
-  //char execd[100];
-  //sprintf(execd, "execd %s\n", myargv[0]);
-  //write(fd,execd,strlen(execd) +1);
- /*
+  /*
+  char execd[100];
+  sprintf(execd, "execd %s\n", myargv[0]);
+  write(fd,execd,strlen(execd) +1);
+ 
   int k=1;
   while(myargv[k]!=NULL)
   { 
@@ -70,14 +78,14 @@ int main(int argc,char** argv)
     write(fd,execd,strlen(execd) +1);
     k++;
   }
-*/
+
+ */
   
   int ret=execv(myargv[0],myargv);
 
   if(ret==-1)
   {
    //write(fd,"execv failed", strlen("execv failed")+1);
-   printf("error: %d\n",errno);
   }
   
 
